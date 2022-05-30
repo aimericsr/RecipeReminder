3a8db75ce3fe5325ea28ce8cbf79cb4eb73e198b
aimericsr <52284219+aimericsr@users.noreply.github.com>
2022-05-30 16:21:02 +0200

    xcodes files

---
+++ RecipeReminder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeReminder
+//
+//  Created by Aimeric Sorin on 30/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeReminder/RecipeReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeReminderApp.swift
+//  RecipeReminder
+//
+//  Created by Aimeric Sorin on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
